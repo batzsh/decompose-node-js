e55b13acefa5eafef1ad3ff398cf38ed7323f575
Henrique Balmant <henrique.balmant@gmail.com>
2021-10-03 18:45:42 -0300

    Add getPrimesFromDivisors method

--- src/classes/Decompose.js
+++ src/classes/Decompose.js
@@ -3,6 +3,17 @@ class Decompose {
     for (let i = 2; i * i <= n; i++) if (n % i === 0) return false;
     return n;
   }
+
+  getPrimesFromDivisors(divisors) {
+    let primes = [];
+
+    for (let n of divisors) {
+      this.isPrime(n) ? primes.push(n) : false;
+    }
+
+    console.log(`Divisores primos: ${primes}`);
+    return primes;
+  }
 }
 
 module.exports = Decompose;
